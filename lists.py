5b6c9b908c405b69afa624830369fa0b247e1b37
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-15 12:26:58 +0600

    practice lists

---
+++ lists.py
@@ -0,0 +1,30 @@
+# my_list = [1, 2, 3]
+# my_list.append(5)
+# print(my_list)
+
+
+# from_range = list(range(5))
+# print(from_range)
+
+
+# Accessing Elements
+# my_list = [10, 20, 30, 40, 50]
+# print(my_list[-1]) 
+# Result: 50
+
+# Adding Element 
+# my_list = [1, 2, 3]
+# my_list.append(4)
+# print(my_list)
+# my_list.insert(2, 10)
+# print(my_list)
+# my_list.extend([100, 200])
+# print(my_list)
+
+
+# Removing elements
+# my_list = [10, 20, 30, 40]
+# my_list.remove(20)
+# print(my_list)
+# my_list.pop(0)
+# print(my_list)
