d9d6d2a33ef5c7577e4c0bc380cbf324831b76c3
ArfanFahad <arfanfahad40@gmail.com>
2025-09-17 23:44:38 +0600

    practice set

---
+++ set.py
@@ -0,0 +1,10 @@
+scraped_tweets = [
+    "I love AI! #innovation",
+    "I love AI! #innovation",
+    "The model is overfitting...",
+    "I love AI! #innovation",
+    "New GPT model released!"
+]
+
+unique_tweets = set(scraped_tweets)
+print(unique_tweets)
