40b5410d4fa5902dbc7e8beea374875023493c6e
ArfanFahad <arfanfahad40@gmail.com>
2025-09-17 11:34:46 +0600

    sorted and sort method

---
+++ sort_methods.py
@@ -0,0 +1,15 @@
+
+# sorted() method doesn't mutate the original lists
+numbers = [2, 4, 5, 1, 33, 22, 1, 99, 23]
+sorted_numbers = sorted(numbers)
+print("Muted sorted lists: ", sorted_numbers)
+print("Original lists: ", numbers)
+
+
+
+
+#sort() Method modifies the original list
+numbers = [8, 20, 1, 98,2, 22, 45, 9, 0]
+sorted_value = numbers.sort()
+print(sorted_value)
+
