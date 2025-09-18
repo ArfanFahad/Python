c6e7406b5fe4dd1ae7e1bb9d9b390c2a98b8b22f
ArfanFahad <arfanfahad40@gmail.com>
2025-09-18 12:03:50 +0600

    Set: practice and understand some methods and concept of set

---
+++ setTwo.py
@@ -0,0 +1,85 @@
+
+# Set Creation
+# fruits = {"apple", "banana", "orange"}
+# print(fruits)
+
+
+#set constructor method 
+# empty = set()
+# print(type (empty))
+
+# From a list 
+# nums = set([1, 22, 33, 33, 33, 4, 9])
+# print(nums)
+
+# lettrs = set("mississpi")
+# print(lettrs)
+
+
+
+# Adding Items to set
+# colors = {"red", "blue"}
+# colors.add("green")
+# colors.add("red")
+# print(colors)
+
+
+# colors = {"red", "green", "blue"}
+# # colors.remove("yellow") # if not found then raise error
+# # an_item_discard = colors.discard("yellow") ans: none
+# # print(an_item_discard)
+# print(colors.pop())
+
+# Membership testing 
+# primes = {2, 3, 5,7, 11, 13}
+# print(7 in primes)
+
+
+# Set Theory Operation 
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5, 6}
+# union_operation = A | B
+# union_method = A.union(B)
+# print(union_operation)
+# print(union_method)
+
+
+# Intersection = Element in both table
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5, 6}
+# raw_intersection = A & B
+# print(raw_intersection)
+# intersection_method = A.intersection(B)
+# print(intersection_method)
+
+
+# Difference = Elements in A but NOT in B
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5, 6}
+# difference_raw = A - B
+# print(difference_raw)
+# difference_method = A.difference(B)
+# print(difference_method)
+
+
+# A = {1, 2, 3, 4}
+# B = {3, 4, 5, 6}
+# C = {1, 2}
+
+# print(C <= A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
