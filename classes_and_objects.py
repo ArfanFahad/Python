7e12d5a8ae4e470bbbf1a17fe4336168a5bd1d93
ArfanFahad <arfanfahad40@gmail.com>
2025-09-22 12:58:57 +0600

    practice classes and object

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,11 +1,33 @@
-class Person:
-    def __init__(self,name,age):
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def greet (self):
+#         print(f"Hello, my name is {self.name}")
+#         print(f"I am {self.age} years old")
+
+# p1 = Person("Fahad", 25)
+# p1.greet()
+
+
+class Station:
+    def __init__(self, name):
         self.name = name
-        self.age = age
+        self.next = None
+    
+    def traverse (head):
+        current = head
 
-    def greet (self):
-        print(f"Hello, my name is {self.name}")
-        print(f"I am {self.age} years old")
+        while (current != None):
+            print(f"Arrived at: {current.name}")
+            current = current.next
 
-p1 = Person("Fahad", 25)
-p1.greet()
+stationA = Station("Station A")
+stationB = Station("Station B")
+
+stationA.next = stationB
+
+Station.traverse(stationA)
+
+    
